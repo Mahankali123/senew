6e2010d0b9a4f82a4b7dfaedc7d5569e7d8cd3ea
Mahankali123 <bestf6302@gmail.com>
2024-02-03 00:34:32 +0530

    java_project

---
+++ src/main/java/SE_project/new_java/App.java
@@ -0,0 +1,13 @@
+package SE_project.new_java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
